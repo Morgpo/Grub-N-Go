9d57ddbf74ef4feaabb5b8f16bb64898bd3c12a5
Morgan <msrupert2002@gmail.com>
2025-10-15 20:01:27 -0500

    Additional FastAPI Endpoints

--- Backend/crud/__init__.py
+++ Backend/crud/__init__.py
@@ -1,1 +1,60 @@
 # CRUD operations package
+
+# Account and Customer CRUD
+from .account_crud import AccountCRUD, CustomerCRUD, RestaurantCRUD
+
+# Address and Payment CRUD
+from .address_crud import AddressCRUD
+from .payment_method_crud import PaymentMethodCRUD
+
+# Menu and Restaurant CRUD
+from .menu_crud import MenuCRUD, MenuItemCRUD
+from .modifier_crud import ModifierCRUD, ModifierOptionCRUD
+from .business_hours_crud import BusinessHoursCRUD
+
+# Order CRUD
+from .order_crud import OrderCRUD, OrderItemCRUD
+
+# Transaction and Refund CRUD
+from .transaction_crud import TransactionCRUD
+from .refund_crud import RefundCRUD
+
+# Price History CRUD
+from .price_history_crud import MenuItemPriceHistoryCRUD
+
+# Audit and Utility CRUD
+from .audit_crud import AuditLogCRUD
+from .utility_crud import UtilityCRUD
+
+__all__ = [
+    # Account and Customer
+    'AccountCRUD',
+    'CustomerCRUD', 
+    'RestaurantCRUD',
+    
+    # Address and Payment
+    'AddressCRUD',
+    'PaymentMethodCRUD',
+    
+    # Menu and Restaurant
+    'MenuCRUD',
+    'MenuItemCRUD',
+    'ModifierCRUD',
+    'ModifierOptionCRUD',
+    'BusinessHoursCRUD',
+    
+    # Order
+    'OrderCRUD',
+    'OrderItemCRUD',
+    
+    # Transaction and Refund
+    'TransactionCRUD',
+    'RefundCRUD',
+    
+    # Price History
+    'MenuItemPriceHistoryCRUD',
+    
+    # Audit and Utility
+    'AuditLogCRUD',
+    'UtilityCRUD'
+]
